8a5c74fed5c3fbdf61b1a8fa7bab6b01128377ac
clevermiraz <miraz.onesoft@gmail.com>
2021-08-08 00:40:11 +0600

    initial release

---
+++ frontend/src/App.jsx
@@ -0,0 +1,23 @@
+import { Container } from 'react-bootstrap';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import Footer from './components/Footer';
+import Header from './components/Header';
+import HomeScreen from './screens/HomeScreen';
+import ProductScreen from './screens/ProductScreen';
+
+function App() {
+    return (
+        <Router>
+            <Header />
+            <main className="py-3">
+                <Container>
+                    <Route exact path="/" component={HomeScreen} />
+                    <Route path="/product/:id" component={ProductScreen} />
+                </Container>
+            </main>
+            <Footer />
+        </Router>
+    );
+}
+
+export default App;
