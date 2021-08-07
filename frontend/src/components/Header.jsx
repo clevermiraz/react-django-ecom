5a0e2b7acd3203b960863714541610f31ba58e24
clevermiraz <miraz.onesoft@gmail.com>
2021-08-08 00:55:31 +0600

    change Page title

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -8,7 +8,7 @@ function Header() {
             <Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect>
                 <Container>
                     <LinkContainer to="/">
-                        <Navbar.Brand>ProShop</Navbar.Brand>
+                        <Navbar.Brand>ProShop:)</Navbar.Brand>
                     </LinkContainer>
 
                     <Navbar.Toggle aria-controls="basic-navbar-nav" />
