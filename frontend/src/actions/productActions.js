4fbfbae701f8ab48cd35e8b7e6516b38da5747c3
clevermiraz <miraz.onesoft@gmail.com>
2021-08-11 02:17:12 +0600

    some changes productActions

--- frontend/src/actions/productActions.js
+++ frontend/src/actions/productActions.js
@@ -22,8 +22,8 @@ export const listProducts = () => async (dispatch) => {
         dispatch({
             type: PRODUCT_LIST_FAIL,
             payload:
-                error.resposne && error.resposne.data.message
-                    ? error.resposne.data.message
+                error.response && error.response.data.message
+                    ? error.response.data.message
                     : error.message
         });
     }
@@ -43,8 +43,8 @@ export const listProductDetails = (id) => async (dispatch) => {
         dispatch({
             type: PRODUCT_DETAILS_FAIL,
             payload:
-                error.resposne && error.resposne.data.message
-                    ? error.resposne.data.message
+                error.response && error.response.data.message
+                    ? error.response.data.message
                     : error.message
         });
     }
