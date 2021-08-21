ce469afddbfa5be0082c6746df36e6b9345bf66d
clevermiraz <miraz.onesoft@gmail.com>
2021-08-21 16:34:47 +0600

    updateUserProfile in Backend

--- backend/api/urls/user_urls.py
+++ backend/api/urls/user_urls.py
@@ -5,10 +5,18 @@ from api.views import user_views as views
 urlpatterns = [
     path('', views.getUsers, name='users'),
 
-    path('login/', views.MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('login/',
+         views.MyTokenObtainPairView.as_view(),
+         name='token_obtain_pair'),
 
     path('register/', views.registerUser, name='register'),
 
-    path('profile/', views.getUserProfile, name='users-profile'),
+    path('profile/',
+         views.getUserProfile,
+         name='users-profile'),
+
+    path('profile/update/',
+         views.updateUserProfile,
+         name='users-profile-update'),
 
 ]
